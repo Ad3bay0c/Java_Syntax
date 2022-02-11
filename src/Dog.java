3f6c1e4e5082fe4dd26db8b45100a05aacd349a2
Ad3bay0c <aclintonadebayo@gmail.com>
2022-02-11 16:07:50 +0100

    implemeted inheritance

--- src/Dog.java
+++ src/Dog.java
@@ -1,2 +1,18 @@
-package src;public class Dog {
+package src;
+
+public class Dog extends Animal{
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private String coat;
+
+    public Dog(String name, int size, int weight, int eyes, int legs, int tail, int teeth, String coat) {
+        super(name, 1, 1, size, weight);
+        this.eyes = eyes;
+        this.tail = tail;
+        this.teeth = teeth;
+        this.legs = legs;
+        this.coat = coat;
+    }
 }
