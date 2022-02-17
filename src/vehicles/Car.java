815d96c12edd86e917f759be0b0a9c7cf3d724c6
Ad3bay0c <aclintonadebayo@gmail.com>
2022-02-17 15:15:55 +0100

    Compositon implemented

--- src/vehicles/Car.java
+++ src/vehicles/Car.java
@@ -1,2 +1,12 @@
-package src.vehicles;public class Car {
+package src.vehicles;
+
+public class Car extends Vehicle{
+    private int doors;
+    private int engineCapacity;
+
+    public Car(String name, int doors, int engineCapacity) {
+        super(name);
+        this.doors = doors;
+        this.engineCapacity = engineCapacity;
+    }
 }
