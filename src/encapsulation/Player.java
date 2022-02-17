56d71ab01d711e4fd2502f97a872736c319bd81b
Ad3bay0c <aclintonadebayo@gmail.com>
2022-02-17 15:14:04 +0100

    Encapsulation practiced

--- src/encapsulation/Player.java
+++ src/encapsulation/Player.java
@@ -1,2 +1,18 @@
-package src.encapsulation;public class Player {
+package src.encapsulation;
+
+public class Player {
+    public String name;
+    public int health;
+    public String weapon;
+
+    public void looseHealth(int damage) {
+        this.health = this.health - health;
+        if (this.health <= 0) {
+            System.out.println("Player Knocked out");
+        }
+    }
+
+    public int healthRemaining() {
+        return this.health;
+    }
 }
