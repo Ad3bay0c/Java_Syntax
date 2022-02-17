89a15cc413eec733722770dc851c2694bcc53055
Ad3bay0c <aclintonadebayo@gmail.com>
2022-02-17 23:39:53 +0100

    implemented autoboxing and unboxing

--- src/autoBoxing/Main.java
+++ src/autoBoxing/Main.java
@@ -1,2 +1,22 @@
-package src.autoBoxing;public class Main {
+package src.autoBoxing;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> intArray = new ArrayList<>();
+
+        for (int i=1; i <=10; i++) {
+            // this is called autoboxing -> converting an int to Integer using valueOf method
+            intArray.add(Integer.valueOf(12));
+        }
+
+        for (int i=1; i <=10; i++) {
+            // this is called unboxing -> converting from Integer back to int
+            System.out.println(i + "------> "+ intArray.get(i).intValue());
+        }
+
+        Integer myIntValue = 26;  // Integer.valueOf(26);
+        int value = myIntValue;  // myIntValue.intValue();
+    }
 }
