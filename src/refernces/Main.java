9c251104c494da83707dc45f190f59e7b578d91e
Ad3bay0c <aclintonadebayo@gmail.com>
2022-02-17 21:03:37 +0100

    implemeted difference between reference and values

--- src/refernces/Main.java
+++ src/refernces/Main.java
@@ -1,2 +1,26 @@
-package src.refernces;public class Main {
+package src.refernces;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int intValue = 25;
+        int myIntValue = intValue;
+
+        System.out.printf("intValue: %d and myIntValue: %d\n", intValue, myIntValue);
+
+        myIntValue = 30;
+        System.out.printf("intValue: %d and myIntValue: %d\n", intValue, myIntValue);
+
+        int[] intArray = new int[5];
+        int[] myIntArray = intArray;
+        System.out.println("Before Insert................");
+        System.out.println("Int Array: " + Arrays.toString(intArray));
+        System.out.println("My Int Array: " + Arrays.toString(myIntArray));
+
+        myIntArray[2] = 32;
+        System.out.println("\nAfter Insert................");
+        System.out.println("Int Array: " + Arrays.toString(intArray));
+        System.out.println("My Int Array: " + Arrays.toString(myIntArray));
+    }
 }
