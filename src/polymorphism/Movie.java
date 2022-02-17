37807ff7e8bbebb3f5575389c47634ef0a885f99
Ad3bay0c <aclintonadebayo@gmail.com>
2022-02-17 15:15:08 +0100

    implemented polymorphism

--- src/polymorphism/Movie.java
+++ src/polymorphism/Movie.java
@@ -1,2 +1,69 @@
-package src.polymorphism;public class Movie {
+package src.polymorphism;
+
+public class Movie {
+    private String name;
+
+    public Movie(String name) {
+        this.name = name;
+    }
+
+    public String plot() {
+        return "No Plot Here";
+    }
+
+    public String getName() {
+        return name;
+    }
 }
+
+class Jaws extends Movie {
+    public Jaws() {
+        super("Jaws");
+    }
+
+    public String plot() {
+        return "A shark eats lots of people";
+    }
+}
+
+class IndependenceDay extends Movie {
+    public IndependenceDay() {
+        super("Independence Day");
+    }
+
+    @Override
+    public String plot() {
+        return "Aliens Attempt to take over by planet earth";
+    }
+}
+
+class MazeRunner extends Movie {
+    public MazeRunner() {
+        super("Maze Runner");
+    }
+
+    @Override
+    public String plot() {
+        return "Kids try to escape the maze";
+    }
+}
+
+class StarWars extends Movie {
+    public StarWars() {
+        super("Star Wars");
+    }
+
+    @Override
+    public String plot() {
+        return "Imperial forces try to take over the universe";
+    }
+}
+
+class ForgettableMovie extends Movie {
+    public ForgettableMovie() {
+        super("Forgettable Movie");
+    }
+
+    // No plot Method
+}
+
