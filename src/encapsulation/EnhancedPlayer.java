a9d78b1c6a50aba0c7715f9e3bd5a8b218cd28e8
Ad3bay0c <aclintonadebayo@gmail.com>
2022-02-17 15:13:31 +0100

    Encapsulation practiced

--- src/encapsulation/EnhancedPlayer.java
+++ src/encapsulation/EnhancedPlayer.java
@@ -1,2 +1,26 @@
-package src.encapsulation;public class EnhancedPlayer {
+package src.encapsulation;
+
+public class EnhancedPlayer {
+    private String name;
+    private int health = 100;
+    private String weapon;
+
+    public EnhancedPlayer(String name, int health, String weapon) {
+        this.name = name;
+        if (health > 0 && health <= 100) {
+            this.health = health;
+        }
+        this.weapon = weapon;
+    }
+
+    public void looseHealth(int damage) {
+        this.health = this.health - health;
+        if (this.health <= 0) {
+            System.out.println("Player Knocked out");
+        }
+    }
+
+    public int getHealth() {
+        return health;
+    }
 }
