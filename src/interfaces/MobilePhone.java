f323ca5503e95064bde81911a42deadfb0867c32
Ad3bay0c <aclintonadebayo@gmail.com>
2022-02-18 18:39:22 +0100

    MobilePhone interface implementation

--- src/interfaces/MobilePhone.java
+++ src/interfaces/MobilePhone.java
@@ -1,2 +1,52 @@
-package src.interfaces;public class MobilePhone {
+package src.interfaces;
+
+public class MobilePhone implements ITelephone{
+    private int myNumber;
+    private boolean isRinging;
+    private boolean isOn = false;
+
+    public MobilePhone(int myNumber) {
+        this.myNumber = myNumber;
+    }
+
+    @Override
+    public void powerOn() {
+        isOn = true;
+        System.out.println("Mobile Phone powered on");
+    }
+
+    @Override
+    public void dial(int phoneNumber) {
+        if (isOn) {
+            System.out.println("Now calling "+phoneNumber+" on desk phone");
+            isRinging = true;
+        } else {
+            System.out.println("Phone is switched off");
+        }
+    }
+
+    @Override
+    public void answer() {
+        if (isRinging) {
+            System.out.println("Answering the Mobile Phone");
+            isRinging = true;
+        }
+    }
+
+    @Override
+    public boolean callPhone(int phoneNumber) {
+        if (phoneNumber == this.myNumber && isOn) {
+            isRinging = true;
+            System.out.println("Ring Ring");
+        } else {
+            isRinging = false;
+            System.out.println("Number not different or phone not on");
+        }
+        return isRinging;
+    }
+
+    @Override
+    public boolean isRinging() {
+        return isRinging;
+    }
 }
