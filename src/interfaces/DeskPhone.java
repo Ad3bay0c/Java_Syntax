76002cce7eb36fd53f9817f2ce7b4c2990fec3db
Ad3bay0c <aclintonadebayo@gmail.com>
2022-02-18 18:38:32 +0100

    DeskPhone interface implementation

--- src/interfaces/DeskPhone.java
+++ src/interfaces/DeskPhone.java
@@ -1,2 +1,45 @@
-package src.interfaces;public class DeskPhone {
+package src.interfaces;
+
+public class DeskPhone implements ITelephone {
+    private int phoneNumber;
+    private boolean isRinging;
+
+    public DeskPhone(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public void powerOn() {
+        System.out.println("No action taken, power button does not exist");
+    }
+
+    @Override
+    public void dial(int phoneNumber) {
+        System.out.println("Now calling "+phoneNumber+" on desk phone");
+        isRinging = true;
+    }
+
+    @Override
+    public void answer() {
+        if (isRinging) {
+            System.out.println("Call Answered");
+            isRinging = true;
+        }
+    }
+
+    @Override
+    public boolean callPhone(int phoneNumber) {
+        if (phoneNumber == this.phoneNumber) {
+            isRinging = true;
+            System.out.println("Ring Ring");
+        } else {
+            isRinging = false;
+        }
+        return isRinging;
+    }
+
+    @Override
+    public boolean isRinging() {
+        return isRinging;
+    }
 }
