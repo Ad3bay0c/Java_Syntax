c5f8b2917e1f9c0f15be6117be416cad96328427
Ad3bay0c <aclintonadebayo@gmail.com>
2022-02-18 18:39:02 +0100

    Itelephone interface implementation

--- src/interfaces/ITelephone.java
+++ src/interfaces/ITelephone.java
@@ -1,2 +1,9 @@
-package src.interfaces;public interface ITelephone {
+package src.interfaces;
+
+public interface ITelephone {
+    void powerOn();
+    void dial(int phoneNumber);
+    void answer();
+    boolean callPhone(int phoneNumber);
+    boolean isRinging();
 }
