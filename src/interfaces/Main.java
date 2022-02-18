4953154f3f9cea1cb007eb087da27deb627247fd
Ad3bay0c <aclintonadebayo@gmail.com>
2022-02-18 18:41:41 +0100

    Main interface implementation

--- src/interfaces/Main.java
+++ src/interfaces/Main.java
@@ -1,2 +1,15 @@
-package src.interfaces;public class Main {
+package src.interfaces;
+
+public class Main {
+    public static void main(String[] args) {
+        ITelephone timPhone = new DeskPhone(1234567);
+        timPhone.powerOn();
+        timPhone.callPhone(12345676);
+        timPhone.answer();
+
+        timPhone = new MobilePhone(12345);
+        //timPhone.powerOn();
+        timPhone.callPhone(34223);
+        timPhone.answer();
+    }
 }
