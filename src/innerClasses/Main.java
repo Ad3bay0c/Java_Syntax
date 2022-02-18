03844381dba15608de33b162a0418a4090abdc01
Ad3bay0c <aclintonadebayo@gmail.com>
2022-02-18 18:36:29 +0100

    Main class implemented to demonstrate inner class

--- src/innerClasses/Main.java
+++ src/innerClasses/Main.java
@@ -1,2 +1,17 @@
-package src.innerClasses;public class Main {
+package src.innerClasses;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GearBox mcLean = new GearBox(6);
+        mcLean.addGear(1, 5.3);
+        mcLean.addGear(2, 10.6);
+        mcLean.addGear(3, 15.9);
+
+        mcLean.operateClutch(true);
+        mcLean.changeGear(1);
+        mcLean.wheelSpeed(1000);
+
+        mcLean.operateClutch(false);
+    }
 }
