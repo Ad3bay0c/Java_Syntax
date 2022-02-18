e48bdf2097f1b896fa7e297df3a1a7e90aa01983
Ad3bay0c <aclintonadebayo@gmail.com>
2022-02-18 18:34:29 +0100

    implemented inner class

--- src/innerClasses/Button.java
+++ src/innerClasses/Button.java
@@ -1,2 +1,26 @@
-package src.innerClasses;public class Button {
+package src.innerClasses;
+
+public class Button {
+    private String title;
+    private OnClickListener onClickListener;
+
+    public Button(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setOnClickListener(OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+    }
+
+    public void onClick() {
+        this.onClickListener.onClick(this.title);
+    }
+
+    public interface OnClickListener {
+        public void onClick(String title);
+    }
 }
