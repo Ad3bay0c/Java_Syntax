7b6c41c0f619fb8f6403dd6c13f3331997aa7341
Ad3bay0c <aclintonadebayo@gmail.com>
2022-02-19 01:16:26 +0100

    implemented abstract classes

--- src/abstractClass/Main.java
+++ src/abstractClass/Main.java
@@ -1,2 +1,17 @@
-package src.abstractClass;public class Main {
+package src.abstractClass;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal dog = new Dog("Yorkie");
+        dog.breath();
+        dog.eat();
+
+        Parrot parrot = new Parrot("Australian ring neck");
+        parrot.breath();
+        parrot.fly();
+        parrot.eat();
+
+        Penguin penguin = new Penguin("Emperor");
+        penguin.fly();
+    }
 }
