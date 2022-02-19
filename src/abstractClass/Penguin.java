3313975f55ab6ecffabd4b37dc6a1753555ee6f5
Ad3bay0c <aclintonadebayo@gmail.com>
2022-02-19 01:16:40 +0100

    implemented abstract classes

--- src/abstractClass/Penguin.java
+++ src/abstractClass/Penguin.java
@@ -1,2 +1,12 @@
-package src.abstractClass;public class Penguin {
+package src.abstractClass;
+
+public class Penguin extends Bird{
+    public Penguin(String name) {
+        super(name);
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("I'm not good at flying, can I swim instead");
+    }
 }
