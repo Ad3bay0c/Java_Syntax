2a22b04bdd2fd790ec80bc0c92805e0a3412f631
Ad3bay0c <aclintonadebayo@gmail.com>
2022-02-19 01:16:03 +0100

    implemented abstract classes

--- src/abstractClass/Bird.java
+++ src/abstractClass/Bird.java
@@ -1,2 +1,19 @@
-package src.abstractClass;public class Bird {
+package src.abstractClass;
+
+public abstract class Bird extends Animal {
+    public Bird(String name) {
+        super(name);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println(getName() + " is pecking");
+    }
+
+    @Override
+    public void breath() {
+        System.out.println("Breathe in, breathe out, repeat");
+    }
+
+    public abstract void fly();
 }
