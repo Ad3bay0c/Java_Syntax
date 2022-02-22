a1f1cde521cf85a43ac2175d1c792a12f080760d
Ad3bay0c <aclintonadebayo@gmail.com>
2022-02-22 21:27:33 +0100

    Generics implementation

--- src/generics/Team.java
+++ src/generics/Team.java
@@ -1,2 +1,59 @@
-package src.generics;public class Team {
+package src.generics;
+
+import java.util.ArrayList;
+
+public class Team<T extends Player> implements Comparable<Team<T>> {
+    private String name;
+    int played = 0;
+    int won = 0;
+    int lost = 0;
+    int tied = 0;
+
+    private ArrayList<T> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean addPlayer(T player) {
+        if (members.contains(player)){
+            System.out.println(player.getName() + " is already in this team");
+            return false;
+        } else {
+            members.add(player);
+            System.out.println(player.getName() + " picked for team "+ this.name);
+            return true;
+        }
+    }
+
+    public int numPlayers() {
+        return this.members.size();
+    }
+
+    public void matchResult(Team opponent, int ourScore, int theirScore) {
+        if (ourScore > theirScore) {
+            won++;
+        }else if (theirScore > ourScore) {
+            lost++;
+        }else {
+            tied++;
+        }
+        played++;
+        if (opponent != null){
+            opponent.matchResult(null, theirScore, ourScore);
+        }
+    }
+
+    public int ranking() {
+        return (won * 2) + tied;
+    }
+
+    @Override
+    public int compareTo(Team<T> team) {
+        return Integer.compare(team.ranking(), this.ranking());
+    }
 }
