6dcd3c91dc288461c13eb2e5c8d5d480fca7bfe6
Ad3bay0c <aclintonadebayo@gmail.com>
2022-02-22 21:26:57 +0100

    Generics implementation

--- src/generics/Main.java
+++ src/generics/Main.java
@@ -1,2 +1,18 @@
-package src.generics;public class Main {
+package src.generics;
+
+public class Main {
+    public static void main(String[] args) {
+        FootballPlayer joe = new FootballPlayer("Joe");
+        BaseballPlayer pat = new BaseballPlayer("Pat");
+        SoccerPlayer tim = new SoccerPlayer("Tim");
+
+        Team<FootballPlayer> naijaTeam = new Team<>("Naija");
+        naijaTeam.addPlayer(joe);
+//        naijaTeam.addPlayer(pat);
+//        naijaTeam.addPlayer(tim);
+        System.out.println(naijaTeam.numPlayers());
+
+        Team<BaseballPlayer> baseballPlayerTeam = new Team<>("Baseball Team");
+        baseballPlayerTeam.addPlayer(pat);
+    }
 }
