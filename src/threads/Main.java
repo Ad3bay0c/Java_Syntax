d7b6c053cf85ce44e1b875053aba5cf92d5c1023
Ad3bay0c <aclintonadebayo@gmail.com>
2022-03-05 00:14:52 +0100

    implemented thread

--- src/threads/Main.java
+++ src/threads/Main.java
@@ -1,2 +1,12 @@
-package src.threads;public class Main {
+package src.threads;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from the main thread");
+
+        Thread anotherThread = new AnotherThread();
+        anotherThread.start();
+
+        System.out.println("Hello again from the main thread");
+    }
 }
